f9cdfa52fc58462d5fe9acd23102fa57316b55f6
aaschuster <aaronjschuster@gmail.com>
2022-12-24 12:21:36 -0500

    add key to comments.js to remove warning

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -10,7 +10,7 @@ const Comments = props => {
     <div>
       {
         comments.map( (comment) => {
-          return < Comment comment={comment}/>;
+          return < Comment comment={comment} key={comment.id}/>;
         })
       }
       {/* map through the comments prop and render a Comment for every piece of data */}
